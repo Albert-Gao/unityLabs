de3e49c4cec3cb4bef1b735e2ba9c218aacb5fa1
Albert Gao <albertgaohy@gmail.com>
2017-01-09 23:29:46 +1300

    Lab01 finished

--- Lab01/Assets/Planet.cs
+++ Lab01/Assets/Planet.cs
@@ -18,7 +18,7 @@ public class Planet : MonoBehaviour {
 	void Update () {
 		// Change in vertical distance 
 		float dy = bobScale * Mathf.Sin(bobRate * Time.time);
-		
+
 		// Move the game object on the vertical axis
 		transform.Translate(new Vector3(0, dy, 0));
 	}
